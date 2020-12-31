6c474fd8724dc4be013f128e73abd3660edf0e1e
Camilo Velandia <velandiacamilo356@gmail.com>
2020-12-30 20:40:22 -0500

    [add] react-helmet

--- src/containers/Checkout.js
+++ src/containers/Checkout.js
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react';
 import { Link } from 'react-router-dom';
 import AppContext from '../context/AppContext';
-
+import {Helmet} from 'react-helmet'
 import '../styles/components/Checkout.css'
 
 const Checkout = () => {
@@ -19,6 +19,30 @@ return sum
 
 } 
  return (
+   <>
+   <Helmet>
+        <title>Merch - Checkout</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@xander_coder" />
+        <meta name="twitter:creator" content="@xander_coder" />
+        <meta name="twitter:title" content="Platzi Conf Store" />
+        <meta name="twitter:description" content="Merch Store" />
+        <meta
+          name="twitter:image"
+          content="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8ZGV2fGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80"
+        />
+        <meta property="og:title" content="Merch Store" />
+        <meta property="og:description" content="Merch Store" />
+        <meta
+          property="og:image"
+          content="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8ZGV2fGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80"
+        />
+        <meta property="og:url" content="https://merch-300216.web.app" />
+        <meta property="og:site_name" content="Merch Store" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="article" />
+        <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
+      </Helmet>
     <div className="Checkout">
       <div className="Checkout-content">
         {cart.length>0 ? <h3>Lista de pedidos</h3> : <h3>sin pedidos...</h3>}
@@ -43,6 +67,7 @@ return sum
       </div>
       )}
     </div>
+    </>
   );
 };
 
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import Products from '../components/Products'
 import initialState from '../initialState'
+import {Helmet} from 'react-helmet'
 
 const Home = () => {
   return (
-    <Products products={initialState.products}/>
+    <>
+      <Helmet>
+        <title>Merch - Productos</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@xander_coder" />
+        <meta name="twitter:creator" content="@xander_coder" />
+        <meta name="twitter:title" content="Platzi Conf Store" />
+        <meta name="twitter:description" content="Merch Store" />
+        <meta
+          name="twitter:image"
+          content="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8ZGV2fGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80"
+        />
+        <meta property="og:title" content="Merch Store" />
+        <meta property="og:description" content="Merch Store" />
+        <meta
+          property="og:image"
+          content="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8ZGV2fGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80"
+        />
+        <meta property="og:url" content="https://merch-300216.web.app" />
+        <meta property="og:site_name" content="Merch Store" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="article" />
+        <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
+      </Helmet>
+      <Products products={initialState.products} />
+    </>
   );
 };
 
